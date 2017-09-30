ef3f2cf568ae1b8edd083ac1c6819a1863cde9ab
psyto <psyhiro@yahoo.co.jp>
2017-09-30 12:11:44 +0900

    walk through video

--- src/main.cpp
+++ src/main.cpp
@@ -266,6 +266,24 @@ int main() {
               ptsy.push_back(prev_car_y);
               ptsy.push_back(car_y);
             }
+            else
+            {
+              // Redefine reference state as as previous path end point
+              ref_x = previous_path_x[prev_size-1];
+              ref_y = previous_path_y[prev_size-1];
+
+              double ref_x_prev = previous_path_x[prev_size-2];
+              double ref_y_prev = previous_path_y[prev_size-2];
+              ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+              // Use two points that make the path tangent to the car
+              ptsx.push_back(ref_x_prev);
+              ptsx.push_back(ref_x);
+
+              ptsy.push_back(ref_y_prev);
+              ptsy.push_back(ref_y);
+              
+            }
 
           	json msgJson;
 
