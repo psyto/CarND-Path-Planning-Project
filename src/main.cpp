2983449e29a2a94b802bd059514430d3571e3b08
psyto <psyhiro@yahoo.co.jp>
2017-10-02 05:17:24 +0900

    walk through video

--- src/main.cpp
+++ src/main.cpp
@@ -197,7 +197,13 @@ int main() {
   	map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  // start in line 1
+  int lane = 1;
+
+  // Have a reference velocity to target
+  double ref_vel = 0; // mph
+
+  h.onMessage([&lane, &ref_vel, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -233,12 +239,6 @@ int main() {
 
           	// Sensor Fusion Data, a list of all other cars on the same side of the road.
           	auto sensor_fusion = j[1]["sensor_fusion"];
-
-            // start in line 1
-            int lane = 1;
-
-            // Have a reference velocity to target
-            double ref_vel = 0; // mph
 
             int prev_size = previous_path_x.size();
 
